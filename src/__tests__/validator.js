964eb3c1919bde34ca785a893f78c5f5d82ea921
Anton9771 <a.livshis@mail.ru>
2023-08-01 08:16:24 +0300

    fixes after review

--- src/__tests__/validator.js
+++ src/__tests__/validator.js
@@ -1,14 +1,29 @@
 import Validator from '../validator';
 
-test.each(["1Arkad1y", "Arkad1y-", "Arkad1111y", "_Arkad1y"])(
-    ("test error name"), (name) => {
-      expect(() => new Validator(name)).toThrow("Неверный формат имени");
-    },
-  );
-  
-  test.each(["Arkad1y", "Ark_ad1y", "A-rk_ad_1y"])(
-    ("test name"), (name) => {
-      const validator = new Validator(name);
-      expect(() => validator.validateUsername().toEqual(name));
-    },
-  );
+test('testing symbols', () => {
+  const validate = new Validator('Аrkad1y');
+  const result = () => validate.validateUserName();
+
+  expect(result).toThrow('Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)');
+});
+
+test('testing right name start ', () => {
+  const validate = new Validator('1Arkad1y');
+  const result = () => validate.validateUserName();
+
+  expect(result).toThrow('Имя не должно начинаться цифрами, символами подчёркивания или тире');
+});
+
+test('testing right name end', () => {
+  const validate = new Validator('Arkad1y-');
+  const result = () => validate.validateUserName();
+
+  expect(result).toThrow('Имя не должно заканчиваться цифрами, символами подчёркивания или тире');
+});
+
+test('testing name with 4 or more num', () => {
+  const validate = new Validator('A1111rkady');
+  const result = () => validate.validateUserName();
+
+  expect(result).toThrow('Имя не должно содержать подряд более трёх цифр');
+});
--- src/validator.js
+++ src/validator.js
@@ -1,13 +1,22 @@
 export default class Validator {
-    constructor(name) {
-      this.name = name;
-      this.validateUsername();
-    };
-    
-    validateUsername() {
-      if (!/^(\w|-)*$/i.test(this.name) || !/^\D*\d{0,3}\D*$/.test(this.name) || !/^[^\d_-].*[^\d_-]$/.test(this.name)) {
-        throw new Error("Неверный формат имени");
-      };
-      return this.name;
-    };
+  constructor(name) {
+    this.name = name;
   }
+
+  validateUserName() {
+    const expectRightUserName = /^[-_\w\d]+$/;
+    const expectRightStartSymbols = /^[^-_\d]/;
+    const expectRightEndSymbols = /.+[^-_\d]$/;
+    const exceptThreeNumbersInRow = /^(?!(.*\d){4,}).*$/;
+
+    if (!expectRightUserName.test(this.name)) {
+      throw new Error('Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9)');
+    } else if (!expectRightStartSymbols.test(this.name)) {
+      throw new Error('Имя не должно начинаться цифрами, символами подчёркивания или тире');
+    } else if (!expectRightEndSymbols.test(this.name)) {
+      throw new Error('Имя не должно заканчиваться цифрами, символами подчёркивания или тире');
+    } else if (!exceptThreeNumbersInRow.test(this.name)) {
+      throw new Error('Имя не должно содержать подряд более трёх цифр1');
+    }
+  }
+}
